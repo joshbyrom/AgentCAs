b144478f9c4794b003a1e5af3eb0f5c1b14af5e9
joshbyrom <joshbyrom@gmail.com>
2012-11-01 16:28:10 -0400

    removed logging and corrected bug in set_width_height

--- src/js/Agents.js
+++ src/js/Agents.js
@@ -2,8 +2,12 @@ var Agent = function(group) {
 	this.group = group;
 	
 	this.get_color = function() { 
-		return this.color; 
+		return this.group.color; 
 	};
+	
+	this.get_group_name = function() {
+		return this.group.name;
+	}
 	
 	this.is_same_group = function(other) {
 		return this.group.name === other.group.name;
--- src/js/CellularAgents.js
+++ src/js/CellularAgents.js
@@ -13,16 +13,17 @@ var shuffle = function(array) {
 
 var View = function(cellspace, canvas_id) {
 	this.cellspace = cellspace;
+	this.canvas_id = canvas_id;
 	
-	this.canvas = document.getElementById(canvas_id);
+	this.canvas = document.getElementById(this.canvas_id);
 	this.context = this.canvas.getContext('2d');
 	
 	this.min_width = 5;
 	this.min_height = 5;
 	
 	this.set_width_height = function() {
-		this.cell_width = this.canvas.width / this.canvas.number_of_columns;
-		this.cell_height = this.canvas.height / this.canvas.number_of_rows;
+		this.cell_width = this.canvas.width / this.cellspace.number_of_columns;
+		this.cell_height = this.canvas.height / this.cellspace.number_of_rows;
 		
 		this.cell_width = Math.max(this.cell_width, this.min_width);
 		this.cell_height = Math.max(this.cell_height, this.min_height);
@@ -33,23 +34,21 @@ var View = function(cellspace, canvas_id) {
 	this.draw_cell = function(cell) {
 		var x = cell.column * this.cell_width;
 		var y = cell.row * this.cell_height;
-		var color = cell.get_state() == 'empty' ? 'white' : cell.agent.get_color();
+		var color = cell.get_state() === 'occupied' ? cell.agent.get_color() : 'black';
 		
 		this.context.fillStyle = color;
 		this.context.lineWidth = 1;
-		this.context.strokeStyle = 'black';
+	    this.context.strokeStyle = 'black';
 		
 		this.context.beginPath();
 		this.context.rect(x, y, this.cell_width, this.cell_height);
 		this.context.closePath();
 		
 		this.context.fill();
-		this.context.stroke();
+		//this.context.stroke();
 	};
 	
 	this.draw = function() {
-		console.log('drawing');
-		
 		var xlen = this.cellspace.number_of_columns;
 		var ylen = this.cellspace.number_of_rows;
 		
@@ -72,7 +71,6 @@ var GroupLogic = function(cellspace, groups) {
 	
 	// update
 	this.update = function() {
-		console.log('updateing...');
 		var xlen = this.cellspace.number_of_columns;
 		var ylen = this.cellspace.number_of_rows;
 		
@@ -87,20 +85,24 @@ var GroupLogic = function(cellspace, groups) {
 		for(x = 0; x < xlen; ++x) {
 			for(y = 0; y < ylen; ++y) {
 				cell = this.cellspace.get_cell_at(x, y);
-				cell.enter_next_state();
+				if(cell.get_state() != cell.next_state) {
+					cell.enter_next_state();
+				}
 			}
 		}
-		console.log('finished...');
 	};
 	
 	this.handle_cell = function(cell) {
 		var state = cell.get_state();
 			
 		if(state === 'unknown') {
-			if(Math.random() < this.birth_change) {
+			if(Math.random() < this.birth_chance) {
 				cell.agent = this.create_agent();
 				cell.next_state = 'occupied';
-			} else cell.next_state = 'empty';
+			} else {
+				cell.next_state = 'empty';
+				cell.agent = null;
+			}
 	    }
 	};
 	
@@ -114,7 +116,7 @@ var main = function(canvas_id) {
 	var Yy = new Group('Yy', 'yellow');
 	var YY = new Group('YY', 'green');
 	
-	var cellspace = new Cellspace(24, 24);
+	var cellspace = new Cellspace(50, 50);
 	
 	var view = new View(cellspace, canvas_id);
 	var logic = new GroupLogic(cellspace, [Yy, YY]);
